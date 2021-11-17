72bed9762216a1ade243170154def20e519a1552
kaitlyn-ott <89671410+kaitlyn-ott@users.noreply.github.com>
2021-11-17 11:27:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('hello');
+
+
+var shopper = {	
+	firstName: "Joe",	
+	lastName: "Smith",	
+	age: 29,	
+	isCurrent: true,	
+	fullname: function() {this.firstName + " " + this.lastName;},
+	groceryCart: ["apples", "oranges", "strawberries", "watermelon", "grapes"]
+
+};
+
+console.log(shopper)
+
+
